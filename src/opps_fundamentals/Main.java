fd8300a9938800511a4c99b67cd74ce74e21203e
Chetan Gandhari <chetangandhari2000@gmail.com>
2026-06-01 04:52:06 +0530

    intro to classes and objects

---
+++ src/opps_fundamentals/Main.java
@@ -0,0 +1,23 @@
+package opps_fundamentals;
+
+/*
+Procedural programing can't do Overloading, Inheritance.
+    * Variables that are needed to be changed and passed through different functions in Procedural programing are tightly coupled i.e, The variable is passed to another functions so-and-so making the data carried by the variable exposed.
+    * No data hiding
+    * No code reusability.
+
+* OOPS: Object oriented programing
+        always think in-terms of Object and there relation with other objects and properties of those objects.
+        * Object: is real word entity with 2 things property, value, state and function, behavior.
+        * Class: It is a blueprint/templet of an object.
+            * From class, we can create multiple Objects.
+            * class defines data variable and data methods.
+
+* */
+
+
+
+
+
+public class Main {
+}
